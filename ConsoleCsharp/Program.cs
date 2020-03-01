cddb56448403b838319e77cbb36b57ec9ab2fb42
SolderingKnowledge <imaikol@yahoo.com>
2020-02-29 19:43:51 -0800

    Add project files.

---
+++ ConsoleCsharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleCsharp
+{
+    public class Program
+    {
+        static void Main()
+        {
+            const string ok = "ok";//const ok = "ok" will not work
+            var o = "o";// static var o = "o" static modifier now allowed for var
+            Console.WriteLine(o);
+            Console.WriteLine(ok);
+
+        }
+    }
+}
